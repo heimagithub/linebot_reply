0f5d4d819e96879ef0f705affccd65a30ff25c3a
heimagithub <heimagooogle@gmail.com>
2018-03-05 17:55:22 +0800

    3rd commit

---
+++ Readme.txt
@@ -0,0 +1,2 @@
+建構 line 回復 送出訊息 Server 端架在 heroku 上面
+SOP：http://heima0809.pixnet.net/blog/post/402653741-180305_LineBot_reply
---
+++ mybot/app.py
@@ -0,0 +1,52 @@
+# encoding: utf-8
+
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+# 填入你的 message api 資訊
+line_bot_api = LineBotApi('Channel access token')
+handler = WebhookHandler('Channel secret')
+
+# 設定你接收訊息的網址，如 https://YOURAPP.herokuapp.com/callback
+# @app.route("/callback", methods=['POST'])
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    print("Request body: " + body, "Signature: " + signature)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
+    # content = "{}: {}".format(event.source.user_id, event.message.text)
+    content = "{}".format( event.message.text)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=content))
+
+import os
+if __name__ == "__main__":
+    app.run()
